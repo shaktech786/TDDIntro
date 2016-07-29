d9213dcd954a4c4f7929723c94d4196087e5cbaf
Shakeel Bhamani <shakeel.bhamani@gmail.com>
2016-07-29 11:57:33 -0400

    All Factorial Tests Pass!

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,7 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i < 0) throw new IllegalArgumentException();
         if (i == 0) return 1;
         return compute(i-1)*i;
     }
